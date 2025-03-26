81f137816d7208f3dbd941be631d292ae06e7201
Pooja Mishra <poojamishra4922@gmail.com>
2025-03-26 14:09:53 +0530

    hfbsfsjf

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "https://poojam4922.github.io/oyelab-react-assesment",
 });
